80805b9375c33fe862553fc8934ac386c4603033
proyecto-eden-3-esferas <121116382+proyecto-eden-3-esferas@users.noreply.github.com>
2026-02-03 09:53:41 +0100

    2026-02-03 09:53 (Entities)

--- Entities.h
+++ Entities.h
@@ -1,8 +1,9 @@
 #ifndef ENTITIES_H
 #define ENTITIES_H
 
-#include <map>
+#include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 
 /* Class Entities<STRING,MAP> contains:
@@ -30,11 +31,15 @@ public:
   void add_entity(       const string_t& key,  const string_t& value) {       entities[key] = value;};
   void add_system_entity(const string_t& key,  const string_t& value) {system_entities[key] = value;};
   //
-  void print_all_entities(          std::ostream& o) const;
+  void print_all_entities(          std::ostream& o) const; // print both internal and external entities
   void declare_doctype_and_entities(std::ostream& o, const string_t& name)                      const;
   void declare_doctype_and_entities(std::ostream& o, const string_t& name, const string_t& dtd) const;
   void print_system_entity(std::ostream& o, const string_t& key,  const string_t& value) const;
   void print_entity(       std::ostream& o, const string_t& key,  const string_t& value) const;
+  //
+  void print_mapped_entity(std::ostream& o, const string_t text) const;
+  void print_transformed(  std::ostream& o, const string_t text) const;
+  static void copy(std::istream& is, std::ostream& os);
   //
   Entities() = default;
   template <typename ITER>
@@ -80,10 +85,60 @@ void Entities<STRING,MAP>::print_system_entity(std::ostream& o, const string_t& 
 
 template <typename STRING, template <typename,typename> typename MAP>
 void Entities<STRING,MAP>::print_entity(std::ostream& o, const string_t& key, const string_t& value) const {
-  o << "<!ENTITY " << key << " \"&" << value << ";\">";
+  //o << "<!ENTITY " << key << " \"&" << value << ";\">";
+  o << "<!ENTITY " << key << " \"" << value << "\">";
 };
 
+template <typename STRING, template <typename,typename> typename MAP>
+void Entities<STRING,MAP>::print_mapped_entity(std::ostream& o, const string_t enti) const {
+  std::cout << "[PROCESSING ENTITY \"" << enti << "\", ";
+  if(entities.contains(       enti))
+  {
+    std::cout << "WHICH IS AN INTERNAL ENTITY.]";
+    o << entities.at(enti);
+  }
+  if(system_entities.contains(enti)) {
+    std::cout << "WHICH IS AN EXTERNAL ENTITY.]";
+    std::ifstream ifs(system_entities.at(enti));
+    if (ifs) {
+    copy(ifs, o);
+    ifs.close();
+    }
+    else
+      std::cout << "\nOpening file \"" << enti << "\" has failed!\n";
+  }
 
+};
+
+template <typename STRING, template <typename,typename> typename MAP>
+void Entities<STRING,MAP>::print_transformed(std::ostream& o, const string_t text) const {
+  for(int i = 0; i < text.length(); ++i) {
+    std::string ent;
+    switch (text[i]) {
+      case '&':
+        ent.clear();
+        ++i;
+        while(text[i] != ';') {
+          ent += text[i];
+          ++i;
+        }
+        print_mapped_entity(o, ent);
+        break;
+      default: o << text[i];
+    }
+  }
+};
+
+template <typename STRING, template <typename,typename> typename MAP>
+void Entities<STRING,MAP>::copy(std::istream& is, std::ostream& os) {
+  char c;
+  while(true) {
+    c = is.get();
+    if(!is.good())
+      break;
+    os << static_cast<char>(c);
+  }
+};
 
 
 #endif
--- Entities.test.cpp
+++ Entities.test.cpp
@@ -14,11 +14,21 @@ int main() {
 
 
   cout << "\n\nNext add some entities and SISTEM entities, then print again:\n";
-  es.add_entity(       "nbsp", "#160");
+  es.add_entity(       "nbsp", "&#160;");
+  es.add_entity(       "date", "January 22nd, 1999");
   es.add_system_entity("posibilidad", "texts/posibilidad.xml");
+  es.add_system_entity("result_file", "../result.txt");
   es.declare_doctype_and_entities(cout,
                                   "book",
                                   "/home/francisco/Documents/docbook.dtd");
+
+  cout << "Let us now test print_transformed(OSTREAM&, TEXT):\n";
+  string str0("Do not break&nbsp;this space!.\n");
+  string str1("Made in &date;.\n");
+  string str2("Computations have produced:\n &result_file;\n, much to our full satisfaction.\n");
+  es.print_transformed(cout, str0);
+  es.print_transformed(cout, str1);
+  es.print_transformed(cout, str2);
 
   return 0;
 }
